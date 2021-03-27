66c4317d9a2022b656a0b7ec605dc9b1036f690b
Aston-Bal <61421482+Aston-Bal@users.noreply.github.com>
2021-03-27 23:41:48 +0000

    Code Annotation for Dialogue scene

--- Assets/Scenes/Dialogue&Endgame/Scripts/Nuaero Scripts/Dialogue.cs
+++ Assets/Scenes/Dialogue&Endgame/Scripts/Nuaero Scripts/Dialogue.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 [System.Serializable]
 
+// The dialogue class will be used as an object that will be passed into the dialogue manager whenever a dialogue is started 
+// This class also host all information that is need for the dialogue.
+
 public class Dialogue
 {
-    public string name;
-    [TextArea(3,15)]
-    public string[] sentences;
+    [TextArea(3, 15)] // The first variable is the minimum amount of lines the text area area will use and the second varibale is the maximum lines in the text area. 
+    public string[] sentences; // Sentencess that will be loaded into the queue.
 }
--- Assets/Scenes/Dialogue&Endgame/Scripts/Nuaero Scripts/DialogueManager.cs
+++ Assets/Scenes/Dialogue&Endgame/Scripts/Nuaero Scripts/DialogueManager.cs
@@ -6,30 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class DialogueManager : MonoBehaviour
 {
-    public Text nameText;   // where the Tank Mania text is displayed in the UI.
-    public Text dialogueText; // where the story line is displayed in the UI
-    public Animator animator;
-    public Queue<string> sentences;
+    public Text dialogueText; // references to UI an where the story line (sentences) will be displayed in the UI
+    public Animator animator; //Animator is used to hide and dispaly the dialogue.
+    private Queue<string> sentences; // All the sentences that will be dispalyed in the dialogue will be held in this queue and as the user reads through the dialog new sentences will be loaded from the end of the queue. 
     public AudioSource continue_Sound; //sound for the audio
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new Queue<string>();
+        sentences = new Queue<string>();  // Initialize the queue
         continue_Sound = GetComponent<AudioSource>();
     }
-    public void StartDialogue (Dialogue dialogue)
+    public void StartDialogue(Dialogue dialogue)
     {
-        animator.SetBool("IsOpen", true);
-        nameText.text = dialogue.name;
+        animator.SetBool("IsOpen", true); // when the start button is clicked a new dialgoue is started and displayed on the scene.
 
-        sentences.Clear();
+        sentences.Clear(); // This clears any pervious sentences in the dialogue.
 
-        foreach (string sentence in dialogue.sentences)
+        foreach (string sentence in dialogue.sentences) //Loops through all the sentences in the array.
         {
-            sentences.Enqueue(sentence);
+            sentences.Enqueue(sentence); // This will queue up a sentence and display the current sentence
         }
-        DisplayNextSentence();
+        DisplayNextSentence(); //This will dispaly the next sentence. The function will also be attached to the continue button.
     }
+
+    // This method will be called from the continue button.
+    // It will first check if there is any more sentencein the queue by using the if statement and if it is 0 it will end the dialogue.
+    // However if there is more sentneces left in the queue, we will get the next sentnece in the queue by using the sentences.Dequeue() and store it in string sentence varibale.
     public void DisplayNextSentence()
     {
         continue_Sound.Play();
@@ -39,23 +41,26 @@ public class DialogueManager : MonoBehaviour
             return;
         }
         string sentence = sentences.Dequeue();
-        StopAllCoroutines();
-        StartCoroutine(TypeSentence(sentence));
+        StopAllCoroutines(); // This make sure that if type sentence is already running it will stop doing so and start a new one.
+        StartCoroutine(TypeSentence(sentence)); // This will start the TypeSentence co-routine.
     }
 
-    IEnumerator TypeSentence (string sentence)
+    // This method is used to display all the characters inisde the sentence one by one.
+    // It will loop through all the individual characters in the sentence to do that we use a foreach loop and a ToCharArray function to convert a string into a character array.
+    // After each letter it will take 0.02f to display the next one.
+    IEnumerator TypeSentence(string sentence)
     {
         dialogueText.text = "";
         foreach (char letter in sentence.ToCharArray())
         {
-            yield return new WaitForSeconds(0.005f);
+            yield return new WaitForSeconds(0.02f);
             dialogueText.text += letter;
         }
     }
 
     void EndDialogue()
     {
-        animator.SetBool("IsOpen", false);
-        SceneManager.LoadScene("MainMenu",LoadSceneMode.Single);
+        animator.SetBool("IsOpen", false); // when the last sentence is displayed and the user clicks the contuine button it close the dialogue scene and move on to the main menu scene. 
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); // It will load the main menu scene once the dialogue scene closes. 
     }
 }
